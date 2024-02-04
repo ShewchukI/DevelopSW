9a04927e2028dd5d742a47fd3efd31f9b2a52b5f
ShewchukI <158152890+ShewchukI@users.noreply.github.com>
2024-02-04 12:05:40 +0200

    Viva MechMat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #print("a")
+def VivaMechMat():
+    return "Viva Mech-mat faculty"
 def Hello():
     return "Hello, world!"
-for i in range(10):
-    print (Hello())
+#for i in range(10):
+#    print (Hello())
+print(VivaMechMat())
